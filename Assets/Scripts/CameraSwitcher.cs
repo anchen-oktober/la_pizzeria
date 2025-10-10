f78ede761e590d5c9fc281b41c928effc728d8ae
Anna G <anchen.oktober@gmail.com>
2025-10-10 12:07:47 +0200

    Initial commit

---
+++ Assets/Scripts/CameraSwitcher.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+public class CameraSwitcher : MonoBehaviour
+{
+    public Camera camera1;
+    public Camera camera2;
+
+    void Start()
+    {
+        camera1.gameObject.SetActive(true);
+        camera2.gameObject.SetActive(false);
+    }
+
+    void Update()
+    {
+        // ПК: клик мышью по объекту
+        if (Input.GetMouseButtonDown(0))
+        {
+            CheckTapOrClick(Input.mousePosition);
+        }
+
+        // Смартфон: тапы
+        if (Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Began)
+        {
+            CheckTapOrClick(Input.touches[0].position);
+        }
+    }
+
+    void CheckTapOrClick(Vector2 screenPosition)
+    {
+        Ray ray = Camera.main.ScreenPointToRay(screenPosition);
+
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (hit.collider.gameObject == gameObject)
+            {
+                ToggleCameras();
+            }
+        }
+    }
+
+    void ToggleCameras()
+    {
+        bool cam1Active = camera1.gameObject.activeSelf;
+
+        camera1.gameObject.SetActive(!cam1Active);
+        camera2.gameObject.SetActive(cam1Active);
+    }
+}
